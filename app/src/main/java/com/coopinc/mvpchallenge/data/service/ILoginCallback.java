1269e1e0c7ad6ea8ff793832a569632e286a92c0
Guybrush <jswoyer@myriadmobile.com>
2016-05-03 09:45:06 -0500

    .gitignore is now working

--- app/src/main/java/com/coopinc/mvpchallenge/data/service/ILoginCallback.java
+++ app/src/main/java/com/coopinc/mvpchallenge/data/service/ILoginCallback.java
@@ -2,9 +2,6 @@ package com.coopinc.mvpchallenge.data.service;
 
 import com.coopinc.mvpchallenge.data.models.MessageModel;
 
-/**
- * Created by joshuaswoyer on 4/20/16.
- */
 public interface ILoginCallback {
     void logInSuccess(MessageModel message);
 
--- app/src/main/java/com/coopinc/mvpchallenge/ui/kingdoms/IKingdomListPresenter.java
+++ app/src/main/java/com/coopinc/mvpchallenge/ui/kingdoms/IKingdomListPresenter.java
@@ -2,8 +2,12 @@ package com.coopinc.mvpchallenge.ui.kingdoms;
 
 public interface IKingdomListPresenter {
     void onPause();
+
     void onResume();
+
     void nextFragment(String kingdomId);
+
     void refresh();
+
     void logout();
 }
